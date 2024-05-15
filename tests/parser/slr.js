840a9e96745a50c27708d1ec1757462f8ba03935
Muhammad Hassan Raza <h.raza1164@yahoo.com>
2024-05-15 21:30:30 +0500

    Added strict type checking to the test case

--- tests/parser/slr.js
+++ tests/parser/slr.js
@@ -25,7 +25,7 @@ exports['test left-recursive nullable grammar'] = function () {
   assert.ok(parser.parse('xxx'), "parse 3 x's")
   assert.ok(parser.parse('x'), 'parse single x')
   assert.throws(function () { parser.parse('y') }, 'throws parse error on invalid token')
-  assert.ok(gen.conflicts == 0, 'no conflicts')
+  assert.ok(gen.conflicts === 0, 'no conflicts')
 }
 
 exports['test right-recursive nullable grammar'] = function () {
@@ -49,7 +49,7 @@ exports['test right-recursive nullable grammar'] = function () {
   parser.lexer = new Lexer(lexData)
 
   assert.ok(parser.parse('xxx'), "parse 3 x's")
-  assert.ok(gen.table.length == 4, 'table has 4 states')
-  assert.ok(gen.conflicts == 0, 'no conflicts')
+  assert.ok(gen.table.length === 4, 'table has 4 states')
+  assert.ok(gen.conflicts === 0, 'no conflicts')
   assert.equal(gen.nullable('A'), true, 'A is nullable')
 }
